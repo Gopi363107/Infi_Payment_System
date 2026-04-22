ed2dea393dc751243b695e031e44c57e2863a088
GOPINATH <ngopi6769@gmail.com>
2026-04-22 08:52:07 +0530

    initial commit

---
+++ src/main/java/org/edu/infi_payment_system/InfiPaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package org.edu.infi_payment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfiPaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfiPaymentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/edu/infi_payment_system/InfiPaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.edu.infi_payment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfiPaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
